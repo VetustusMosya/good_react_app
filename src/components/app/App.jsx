b0d47382bfd6d1b1652fd87767aa22f99673946a
Moiseev <moiseevsenya2002@gmail.com>
2022-10-25 19:48:01 +0300

    logic & adaptation done

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,29 +1,51 @@
 import { useState } from "react";
-import Random from "../random";
+import RenderAbuse from "../renderAbuse";
 import CangerAbuse from "../button/button";
 
 import styles from "./App.module.css";
 
 export default function App() {
   let [count, setCount] = useState(1);
-  // const [enough, changeEnough] = useState();
+  let [num, changeNum] = useState(+localStorage.getItem("int") || Random());
+  let [todayDay] = useState(new Date().toDateString());
+  let [oldDay] = useState(localStorage.getItem("date") || "poh");
+
+  function Random() {
+    let randomInt = Math.floor(Math.random() * 4) + 1;
+    if (randomInt !== +localStorage.getItem("int")) {
+      localStorage.setItem("int", randomInt);
+      return randomInt;
+    } else {
+      localStorage.setItem("int", randomInt);
+      return Random();
+    }
+  }
 
   const ChangeAbuse = () => {
     setCount(++count);
+    changeNum(Random());
+    localStorage.setItem("date", new Date().toDateString());
+  };
+
+  const CheckRender = () => {
+    if (count < 4) {
+      if (oldDay !== todayDay) {
+        return <CangerAbuse event={ChangeAbuse}>Change my name</CangerAbuse>;
+      }
+    }
+    return (
+      <>
+        <h2>until midnight.</h2>
+        <p>U have to wait.</p>
+      </>
+    );
   };
 
   return (
     <div className={styles.app}>
       <h1>Today u are</h1>
-      <Random />
-      {count < 4 ? (
-        <CangerAbuse event={ChangeAbuse}>Change my name</CangerAbuse>
-      ) : (
-        <>
-          <h2>until midnight.</h2>
-          <p>U have to wait.</p>
-        </>
-      )}
+      <RenderAbuse num={num} />
+      {CheckRender()}
     </div>
   );
 }
--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,7 +1,5 @@
 import "./button.module.css";
 
 export default function Button(props) {
-  return (
-    <button onClick={() => props.event(props.id)}>{props.children}</button>
-  );
+  return <button onClick={props.event}>{props.children}</button>;
 }
